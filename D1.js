4c45b004bae44ed14805142f8657fa8e7113328d
Philip <philedu30@gmail.com>
2024-01-22 16:26:06 +0100

    U1-W2-D1

--- D1.js
+++ D1.js
@@ -61,11 +61,10 @@ console.log(myConstant);
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
-/*
-let x = 12;
-let result  = 4 - x;
-console-log(result);
-*/
+
+x = 12;
+let result = 4 - x;
+console - log(result);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
